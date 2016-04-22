7e2516a62c69170e4251b99e345365c9c9337794
Kris Foss <kris.foss@gmail.com>
2016-04-22 09:49:21 -0600

    nearly nearly

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,21 +1,18 @@
+require 'pry'
 VERSION = 3
 
 class Complement
 
-  # def initialize
-  #   @complements_hash =
-  #     { 'G' => 'C',
-  #       'C' => 'G',
-  #       'T' => 'A',
-  #       'A' => 'U'}
-  # end
-
   def self.of_dna(x)
     strand = x.chars
-    if complements_hash.keys.include?(strand)
-      strand.map {|letter| complements_hash.fetch(letter)}.join
+    # binding.pry
+    valid = strand.select {|char| complements_hash.has_key?(char)}
+
+    if valid == []
+      raise ArgumentError.new("Invalid Character")
+
     else
-      ArgumentError
+    valid.map {|letter| complements_hash.fetch(letter)}.join
     end
   end
 
